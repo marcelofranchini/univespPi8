4d02b5d2583ddd4f24fed68af74861e0293f920a
Marcelo Franchini <marcelo_franchini@hotmail.com>
2022-04-22 16:38:18 -0300

    configuracao

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <Text>dkjdjkf</Text>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text> isduhsuidhsiu</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
